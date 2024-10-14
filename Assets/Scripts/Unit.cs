2ca4fe0383017213923ae38f41cb577f1651955c
LanaRitz <milanaritz45@gmail.com>
2024-10-14 09:57:08 +0300

    Сollisions between objects

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -3,17 +3,40 @@ using UnityEngine;
 public class Unit : MonoBehaviour
 {
     private MovementController movementController;
-    public bool IsCat;
 
     void Start()
     {
         movementController = GetComponent<MovementController>();
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider collider)
     {
-        
+        if (collider.gameObject.GetComponent<Unit>() == null) return;
+
+        Unit otherUnit = collider.gameObject.GetComponent<Unit>();
+
+        if(this.CompareTag("Cat") && otherUnit.CompareTag("Cat"))
+        {
+            Die();
+        }
+        else if (this.CompareTag("Mouse") && otherUnit.CompareTag("Cat"))
+        {
+            Die();
+        }
+        else if (this.CompareTag("Mouse") && otherUnit.CompareTag("Mouse"))
+        {
+            Ignore(otherUnit);
+        }
     }
 
+    private void Ignore(Unit otherUnit)
+    {
+        Physics.IgnoreCollision(this.GetComponent<Collider>(), otherUnit.GetComponent<Collider>());
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
 
 }
